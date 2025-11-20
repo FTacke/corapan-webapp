4052883a5dfd734a2373cf6db9f513c08e999a84
Felix Tacke <felix.tacke@uni-marburg.de>
2025-11-20 11:24:57 +0100

    Remove legacy static assets

--- static/js/modules/corpus/api.js
+++
@@ -1,4 +0,0 @@
-// Deprecated replacement for the corpus API helper
-console.warn('[DEPRECATED] js/modules/corpus/api.js is deprecated; use js/modules/search/api.js or advanced_api endpoints');
-
-export default {};
--- static/js/modules/corpus/init.js
+++
@@ -1,8 +0,0 @@
-/**
- * Corpus Progressive Enhancement
- * This file is NO LONGER NEEDED - corpus/index.js handles all initialization
- * Kept for backwards compatibility but does nothing
- */
-
-console.log('[Corpus Init] Module loaded (deprecated - using index.js instead)');
-
