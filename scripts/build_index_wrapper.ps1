15bee2ce42a03b0500cd0249571836eca2194f6f
Felix Tacke <felix.tacke@uni-marburg.de>
2025-11-20 11:24:57 +0100

    Remove legacy scripts

--- scripts/build_index_wrapper.ps1
+++
@@ -1,7 +0,0 @@
-# Temporary wrapper to run build script with Java in PATH
-$env:Path = "C:\Program Files\Microsoft\jdk-17.0.17.10-hotspot\bin;$env:Path"
-
-Write-Host "Java verfuegbar: $((Get-Command java -ErrorAction SilentlyContinue).Source)" -ForegroundColor Green
-Write-Host ""
-
-.\scripts\build_blacklab_index.ps1 -Force
